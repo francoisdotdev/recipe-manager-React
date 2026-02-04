8c99392ce51b88c7d0ca1afa97d4a3753b12c994
francoisdotdev <lef02fran@gmail.com>
2026-02-04 18:42:14 +0100

    feat: add RecipePage component to display recipe details

---
+++ src/pages/RecipePage.tsx
@@ -0,0 +1,9 @@
+import { RecipeDetail } from '../components/recipe/RecipeDetail'
+
+interface RecipePageProps {
+  recipeId: number
+}
+
+export function RecipePage({ recipeId }: RecipePageProps) {
+  return <RecipeDetail recipeId={recipeId} />
+}
